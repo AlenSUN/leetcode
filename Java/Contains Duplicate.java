95a0973aad829c15ed8a2349f40acbc2698e42c4
AlenSUN <304136198@qq.com>
2016-03-10 16:21:59 +0800

    Format

--- Java/Contains Duplicate.java
+++ Java/Contains Duplicate.java
@@ -1,27 +1,27 @@
 public class Solution {
 	public boolean containsDuplicate(int[] nums) {
 		// version 1
-//    	Set<Integer>[] s = new HashSet[10];
-//    	for (int i = 0; i < 10; i++)
-//    		s[i] = new HashSet<>();
-//    	for (int i = 0; i < nums.length; i++)
-//    		if (!s[Math.abs(nums[i] % 10)].add(nums[i]))
-//    			return true;
+//		Set<Integer>[] s = new HashSet[10];
+//		for (int i = 0; i < 10; i++)
+//			s[i] = new HashSet<>();
+//		for (int i = 0; i < nums.length; i++)
+//			if (!s[Math.abs(nums[i] % 10)].add(nums[i]))
+//				return true;
 //		return false;
-    	
-    	// version 2
-//    	Set<Integer> s = new HashSet<>();
-//      for (int i = 0; i < nums.length; i++)
-//      	if (!s.add(nums[i]))
-//      		return true;
+
+		// version 2
+//		Set<Integer> s = new HashSet<>();
+//		for (int i = 0; i < nums.length; i++)
+//			if (!s.add(nums[i]))
+//				return true;
 //		return false;
-    	
-    	// version 3
-    	Set<Integer> s = new HashSet<>();
-        int l = nums.length;
-        for (int i = 0; i < l; i++)
-        	if (!s.add(nums[i]))
-        		return true;
-        return false;
-    }
+
+		// version 3
+		Set<Integer> s = new HashSet<>();
+		int l = nums.length;
+		for (int i = 0; i < l; i++)
+			if (!s.add(nums[i]))
+				return true;
+		return false;
+	}
 }
