03a5eba49fa640f6d1dfd1c1e30c8031eb5e5239
AlenSUN <304136198@qq.com>
2016-04-05 16:10:38 +0800

    Update

--- Java/Majority Element.java
+++ Java/Majority Element.java
@@ -1,24 +1,17 @@
-import java.util.Map.Entry;
-
+// use Moore's voting algorithm
 public class Solution {
 	public int majorityElement(int[] nums) {
-		Map<Integer, Integer> m = new HashMap<>();
-		int l = nums.length;
-		for (int i = 0; i < l; i++)
-			if (m.containsKey(nums[i]))
-				m.put(nums[i], m.get(nums[i]) + 1);
-			else
-				m.put(nums[i], 1);
-		Set<Entry<Integer, Integer>> se = m.entrySet();
-		Iterator<Entry<Integer, Integer>> i = se.iterator();
-		int max = 0, maj = 0;
-		while (i.hasNext()) {
-			Entry<Integer, Integer> e = i.next();
-			if (e.getValue() > max) {
-				max = e.getValue();
-				maj = e.getKey();
+		int maj = nums[0], vote = 1, l = nums.length;
+		for (int i = 1; i < l; i++)
+			if (vote != 0) {
+				if (nums[i] == maj)
+					vote++;
+				else
+					vote--;
+			} else {
+				maj = nums[i];
+				vote = 1;
 			}
-		}
 		return maj;
 	}
 }
