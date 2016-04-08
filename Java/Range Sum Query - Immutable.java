33aab2711785758382a43eff42b24891113b954f
AlenSUN <304136198@qq.com>
2016-04-08 18:37:00 +0800

    use caching

--- Java/Range Sum Query - Immutable.java
+++ Java/Range Sum Query - Immutable.java
@@ -1,41 +1,19 @@
+// dynamic programming, use caching
 public class NumArray {
-	private int[] nums;
 	private int[] sums;
-	private int sqrt;
 
 	public NumArray(int[] nums) {
-		this.nums = nums;
 		int l = nums.length;
-		if (l == 0)
-			return;
-		sqrt = (int) Math.sqrt(l);
-		sums = new int[l / sqrt + 1];
-		int k = 0;
-		for (int i = 0; i < l; i += sqrt) {
-			int sum = 0;
-			for (int j = i; j < i + sqrt && j < l; j++)
-				sum += nums[j];
-			sums[k++] = sum;
+		if (l > 0) {
+			sums = new int[l];
+			sums[0] = nums[0];
+			for (int i = 1; i < l; i++)
+				sums[i] = sums[i - 1] + nums[i];
 		}
 	}
 
 	public int sumRange(int i, int j) {
-		if (i / sqrt == j / sqrt) {
-			int sum = 0;
-			for (int x = i; x <= j; x++)
-				sum += nums[x];
-			return sum;
-		}
-
-		int a = i / sqrt + 1, b = j / sqrt - 1;
-		int sum = 0;
-		for (int x = a; x <= b; x++)
-			sum += sums[x];
-		for (int x = i; x < a * sqrt && x < nums.length; x++)
-			sum += nums[x];
-		for (int x = (b + 1) * sqrt; x <= j; x++)
-			sum += nums[x];
-		return sum;
+		return i == 0 ? sums[j] : sums[j] - sums[i - 1];
 	}
 }
 
