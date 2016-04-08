515e3311476a0f9599d2ffef3e8cf356ba91ea65
AlenSUN <304136198@qq.com>
2016-04-08 10:45:19 +0800

    use two pointers

--- Java/Intersection of Two Linked Lists.java
+++ Java/Intersection of Two Linked Lists.java
@@ -9,19 +9,31 @@
  *     }
  * }
  */
-// use hash table
+// use two pointers, see the approach #3 of editorial solution
+// brief explanation: a + c + b = b + c + a 
 public class Solution {
 	public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-		Set<ListNode> s = new HashSet<>();
-		while (headA != null) {
-			s.add(headA);
-			headA = headA.next;
+		if (headA == null || headB == null)
+			return null;
+
+		ListNode nodeA = headA, nodeB = headB;
+		ListNode tailA = null, tailB = null;
+		while (nodeA != nodeB) {
+			if (nodeA.next == null) {
+				tailA = nodeA;
+				if (tailB != null && tailA != tailB)
+					return null;
+				nodeA = headB;
+			} else
+				nodeA = nodeA.next;
+			if (nodeB.next == null) {
+				tailB = nodeB;
+				if (tailA != null && tailA != tailB)
+					return null;
+				nodeB = headA;
+			} else
+				nodeB = nodeB.next;
 		}
-		while (headB != null) {
-			if (s.contains(headB))
-				return headB;
-			headB = headB.next;
-		}
-		return null;
+		return nodeA;
 	}
 }
