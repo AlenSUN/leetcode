844b36842c9460f246e752376b978641e6ac81f2
AlenSUN <304136198@qq.com>
2016-04-14 14:28:24 +0800

    add iterative version

--- Java/Invert Binary Tree.java
+++ Java/Invert Binary Tree.java
@@ -9,12 +9,30 @@
  */
 public class Solution {
 	public TreeNode invertTree(TreeNode root) {
+		// Approach #1 (Recursive)
 		if (root == null)
 			return null;
 		TreeNode left = invertTree(root.right);
 		TreeNode right = invertTree(root.left);
 		root.left = left;
 		root.right = right;
+		return root;
+
+		// Approach #2 (Iterative)
+		if (root == null)
+			return null;
+		Queue<TreeNode> q = new LinkedList<>();
+		q.add(root);
+		while (!q.isEmpty()) {
+			TreeNode node = q.remove();
+			TreeNode tmp = node.left;
+			node.left = node.right;
+			node.right = tmp;
+			if (node.left != null)
+				q.add(node.left);
+			if (node.right != null)
+				q.add(node.right);
+		}
 		return root;
 	}
 }
--- Java/Maximum Depth of Binary Tree.java
+++ Java/Maximum Depth of Binary Tree.java
@@ -9,10 +9,32 @@
  */
 public class Solution {
 	public int maxDepth(TreeNode root) {
+		// recursive
 		if (root == null)
 			return 0;
 		int l = maxDepth(root.left);
 		int r = maxDepth(root.right);
 		return l > r ? l + 1 : r + 1;
+
+		// BFS iterative
+		if (root == null)
+			return 0;
+		Queue<TreeNode> q0 = new LinkedList<>();
+		Queue<TreeNode> q1 = new LinkedList<>();
+		q1.add(root);
+		int d = 0;
+		while (!q1.isEmpty()) {
+			d++;
+			while (!q1.isEmpty())
+				q0.add(q1.remove());
+			while (!q0.isEmpty()) {
+				TreeNode node = q0.remove();
+				if (node.left != null)
+					q1.add(node.left);
+				if (node.right != null)
+					q1.add(node.right);
+			}
+		}
+		return d;
 	}
 }
